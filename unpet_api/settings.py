6280b833fac428eec944ae233d0ea9707060a2fc
CesarFRR <crinconro@unal.edu.co>
2023-09-18 17:22:03 -0500

    'changes to render deploy'

--- unpet_api/settings.py
+++ unpet_api/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'pets',
     'rest_framework',
-    'corsheaders',
-    'coreapi',
+
+    'coreapi'
 ]
 
 MIDDLEWARE = [
