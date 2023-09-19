e17a58da48314a3900089a27919c128b40848636
CesarFRR <crinconro@unal.edu.co>
2023-09-18 21:53:59 -0500

    render fix final

--- unpet_api/settings.py
+++ unpet_api/settings.py
@@ -152,6 +152,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # CORS URL AUTORIZADAS
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "https://unpet-der5.onrender.com"]
 
 REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema"}
