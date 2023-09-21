aec8edc81f154e247a55c6bf6a6aa02b27ffd032
CesarFRR <crinconro@unal.edu.co>
2023-09-20 20:50:39 -0500

    firebase connection added Español: -> Se agregó una conection a firebase storage, esto es un tipo de base de datos a manera de folder en la nube, de modo que se puede subir cualquier tipo de archivo y se puede acceder a él por un enlace generado, este enlace se puede guardar en la base de datos para luego ser accedido en el frontend sin ningun problema como es el caso del atributo urlFoto de la tabla Persona -> Se organizó las carpetas a modo de seguir la presentacion de un patron de diseño de arquitectura: CLLIENTE <---> SERVIDOR

---
+++ server/firebase/firebase2.py
@@ -0,0 +1,42 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage as fstorage
+import os
+
+# Establece la variable de entorno GOOGLE_APPLICATION_CREDENTIALS
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./server/firebase/serviceAccountKey.json"
+serviceAccount = "./server/firebase/serviceAccountKey.json"
+
+def uploadUserIMG():
+    pass
+
+def getUserIMG():
+    pass
+
+def deleteUserIMG():
+    pass
+
+def __isJPG(url: str):
+    return url.lower.endswith('.jpg')
+
+cred = credentials.Certificate(serviceAccount)
+
+app = firebase_admin.initialize_app(cred, {'storageBucket': 'unpet-f599b.appspot.com'})
+
+bucket = fstorage.bucket()
+blob = bucket.blob("nuevoGato2")  # Cambia el nombre de acuerdo a tu preferencia
+#blob.upload_from_file()
+blob.upload_from_filename('./server/public/img/gato.jpg')
+
+
+# Obtiene la URL de descarga de la imagen
+url_imagen = blob.public_url
+
+# Imprime la URL o utilízala según tus necesidades
+print("URL de la imagen:", url_imagen)
+
+# Genera una URL firmada para el objeto de almacenamiento sin tiempo de expiración
+url_firmada = blob.generate_signed_url(expiration=3153600000)  #100 años, basicamente infinito
+
+# Imprime la URL firmada o utilízala según tus necesidades
+print("URL de la imagen firmada:", url_firmada)
