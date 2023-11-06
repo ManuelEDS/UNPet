0b865e69a723b6fb01878c6bec427aa191e37118
CesarFRR <crinconro@unal.edu.co>
2023-11-06 16:46:22 -0500

    variables changed to deploy on render

--- backend/unpet_api/settings.py
+++ backend/unpet_api/settings.py
@@ -28,10 +28,10 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG=True
-LOCAL_DB = True # DEBUG = True #para usar sqlite, FALSE para la db con las variables de entorno
+DEBUG=False
+LOCAL_DB = False # DEBUG = True #para usar sqlite, FALSE para la db con las variables de entorno
 DOCKER_MODE=False
-RENDER_MODE = False
+RENDER_MODE = True
 if DOCKER_MODE: # Modo: despliegue en algun seguidor docker (plan B por si render falla)
     ALLOWED_HOSTS = ['http://localhost:81']
     CORS_ALLOWED_ORIGINS = []
@@ -40,6 +40,8 @@ elif RENDER_MODE: # Modo: despliegue en render
     CORS_ALLOW_CREDENTIALS = True
     ALLOWED_HOSTS = ['127.0.0.1','https://unpet-web.onrender.com',]
     CORS_ALLOWED_ORIGINS = ['https://unpet-web.onrender.com',]
+    CSRF_TRUSTED_ORIGINS = ['https://unpet-web.onrender.com',]
+
 else: # Modo: desarrollo en localhost
     CORS_ALLOW_CREDENTIALS = True
     ALLOWED_HOSTS = ['localhost', 'localhost:5173', '127.0.0.1']
