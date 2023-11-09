e045e22bb83a1b5af5ddec15cd55c7be0dcf934d
davidrt <jdramirezt@unal.edu.co>
2023-11-09 05:00:31 -0500

    donations fix

--- frontend/src/components/Donations.jsx
+++ frontend/src/components/Donations.jsx
@@ -1,11 +1,10 @@
-import * as React from 'react';
 import { Fragment } from 'react';
 import { FaCreditCard } from 'react-icons/fa';
 
 export function Donations() {
     return (
         <Fragment>
-            <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+            <div className="maxn-h-screen bg-gray-100 py-10 flex flex-col justify-center sm:py-35">
                 <div className="relative py-3 sm:max-w-xl sm:mx-auto">
                     <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
                     <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
@@ -14,58 +13,32 @@ export function Donations() {
                                 <FaCreditCard className="mx-auto h-12 w-auto text-indigo-500" />
                                 <h2 className="text-center text-3xl font-extrabold text-gray-900">Ayúdanos a dar más oportunidades</h2>
                             </div>
-                            <div className="divide-y divide-gray-200">
+                            <div className="divide-y divide-gray-200 align-center">
                                 <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                                     <p>
                                         Si deseas hacer una donación, puedes hacerlo a través de los siguientes medios:
                                     </p>
-                                    <ul className="list-disc space-y-2">
+                                    <ul className="list-disc space-y-2 align-center">
                                         <li className="flex items-start">
-                                            <div className="flex-shrink-0">
-                                                <svg className="h-6 w-6 text-green-500" viewBox="0 0 24 24" fill="none">
-                                                    <path
-                                                        d="M12 22C16.4183 22 20 18.4183 20 14C20 9.58172 16.4183 6 12 6C7.58172 6 4 9.58172 4 14C4 18.4183 7.58172 22 12 22Z"
-                                                        stroke="currentColor"
-                                                        strokeWidth="2"
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                    />
-                                                    <path
-                                                        d="M22 12H18L15 21L9 3L6 12H2"
-                                                        stroke="currentColor"
-                                                        strokeWidth="2"
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                    />
-                                                </svg>
+                                            <div className="max-w-md mx-auto">
+                                                <img src="https://i.imgur.com/PWBTXDu.png" href="https://paypal.me/UNPetDonaciones" width="200px" alt="Nequi" />
                                             </div>
+                                        </li>
+                                        <li className="flex items-start">
                                             <p className="ml-3 text-green-500">
-                                                <a href="https://paypal.me/UNPetDonaciones" target="_blank" rel="noopener noreferrer">
+                                                <a className="max-w-md mx-auto" href="https://paypal.me/UNPetDonaciones" target="_blank" rel="noopener noreferrer">
                                                     Paypal
                                                 </a>
                                             </p>
                                         </li>
-                                        <li className="flex items-start">
+                                        <li className="flex items-start justify-center">
                                             <div className="flex-shrink-0">
-                                                <svg className="h-6 w-6 text-green-500" viewBox="0 0 24 24" fill="none">
-                                                    <path
-                                                        d="M12 22C16.4183 22 20 18.4183 20 14C20 9.58172 16.4183 6 12 6C7.58172 6 4 9.58172 4 14C4 18.4183 7.58172 22 12 22Z"
-                                                        stroke="currentColor"
-                                                        strokeWidth="2"
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                    />
-                                                    <path
-                                                        d="M22 12H18L15 21L9 3L6 12H2"
-                                                        stroke="currentColor"
-                                                        strokeWidth="2"
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                    />
-                                                </svg>
+                                                <img src="https://i.imgur.com/lxXHnCW.png" width="200px" alt="Nequi" />
                                             </div>
-                                            <p className="ml-3 text-green-500">
-                                                <a href="https://www.nequi.com.co/" target="_blank" rel="noopener noreferrer">
+                                        </li>
+                                        <li className="flex items-start align-center">
+                                            <p className="text-center ml-3 text-green-500">
+                                                <a target="_blank" rel="noopener noreferrer">
                                                     Nequi
                                                 </a>
                                             </p>
@@ -77,7 +50,7 @@ export function Donations() {
                     </div>
                 </div>
             </div>
-        </Fragment>
+        </Fragment >
     );
 }
 
