ac76c1e43850893bbfb7ddf1e44b58fe9b2ec3de
davidrt <jdramirezt@unal.edu.co>
2023-11-09 05:11:01 -0500

    left bar fix

--- frontend/src/components/LeftBar.jsx
+++ frontend/src/components/LeftBar.jsx
@@ -1,5 +1,5 @@
 
-import { FaHome, FaHashtag, FaStar, FaCog, FaUserCircle } from 'react-icons/fa';
+import { FaHome, FaHashtag, FaQuestion, FaCog, FaUserCircle } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 
 function LeftBar() {
@@ -20,6 +20,22 @@ function LeftBar() {
                             <FaHashtag />
                         </IconContext.Provider>
                         Tendencias
+                    </a>
+                </li>
+                <li className="text-gray-600 hover:text-gray-900 hover:bg-gray-100 rounded-md py-2 px-5 flex items-center justify-start">
+                    <a href="/quienes-somos" className='py-2 px-3 flex items-center'>
+                        <IconContext.Provider value={{ className: 'h-5 w-5 inline-block mr-2' }}>
+                            <FaUserCircle />
+                        </IconContext.Provider>
+                        Sobre nosotros
+                    </a>
+                </li>
+                <li className="text-gray-600 hover:text-gray-900 hover:bg-gray-100 rounded-md py-2 px-5 flex items-center justify-start">
+                    <a href="/legal/terms-and-conditions" className='py-2 px-3 flex items-center'>
+                        <IconContext.Provider value={{ className: 'h-5 w-5 inline-block mr-2' }}>
+                            <FaQuestion />
+                        </IconContext.Provider>
+                        Términos del servicio
                     </a>
                 </li>
                 <li className="text-gray-600 hover:text-gray-900 hover:bg-gray-100 rounded-md py-2 px-5 flex items-center justify-start">
