1cb21b9a18f29ee18229867c41d4f850a549952f
davidrt <jdramirezt@unal.edu.co>
2023-11-08 21:07:43 -0500

    server deploy

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -4,9 +4,9 @@ import axios from "axios";
 
 //===============================================//
 // --> SOLO UNA PUEDE SER TRUE <-- //
-export const DEBUG = true;
+export const DEBUG = false;
 export const DOCKER_MODE = false;
-export const RENDER_MODE = false;
+export const RENDER_MODE = true;
 //===============================================//
 export const CREDENTIALS = DOCKER_MODE ? 'same-origin' : 'include';
 let URL = "";
