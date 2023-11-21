680da4742bc20bf070e4ee9961a46e2beb0496dc
davidrt <jdramirezt@unal.edu.co>
2023-11-20 21:28:49 -0500

    footer rfix 4

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,11 +5,8 @@ export default function Footer() {
     <footer className="bg-green-600 text-white py-2 px-6 bottom-0 w-full fixed z-50" >
       <div className="max-w-7xl mx-auto text-center">
         <div className="mt-2 flex justify-center">
-          <p className="text-sm " className="mr-4">
+          <p className="text-sm mr-4">
             Síguenos en redes sociales:
-          </p>
-          <p className="text-sm">
-
           </p>
           <a href="https://twitter.com/UNPet2023" target="_blank" rel="noopener noreferrer" className="mr-4">
             <FaTwitter className="h-6 w-6" />
